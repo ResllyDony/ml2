48ce89ae634ac2eb433298ff7936293d6a8cbab9
ResllyDony <83096537+ResllyDony@users.noreply.github.com>
2024-02-22 17:26:07 -0700

    added run script

--- assignment.py
+++ assignment.py
@@ -1,4 +1,4 @@
-import mlrose
+import mlrose_hiive as mlrose
 import numpy as np
 
 # Define functions to create optimization problems
@@ -34,27 +34,37 @@ def experiment3():
     problem = mlrose.DiscreteOpt(length=100, fitness_fn=fitness, maximize=True, max_val=2)
     return problem
 
+def main():
+    # Define optimization algorithms
+    algorithms = {
+        "Random Hill Climbing": mlrose.random_hill_climb,
+        "Simulated Annealing": mlrose.simulated_annealing,
+        "Genetic Algorithm": mlrose.genetic_alg,
+        "MIMIC": mlrose.mimic
+    }
 
-# Define optimization algorithms
-algorithms = {
-    "Random Hill Climbing": mlrose.random_hill_climb,
-    "Simulated Annealing": mlrose.simulated_annealing,
-    "Genetic Algorithm": mlrose.genetic_alg,
-    "MIMIC": mlrose.mimic
-}
+    # Loop through problems and algorithms
+    problems = [experiment1, experiment2, experiment3]
 
-# Loop through problems and algorithms
-problems = [experiment1, experiment2, experiment3]
-
-for problem_func in problems:
-    problem = problem_func()
-    print(f"Problem: {problem.__class__.__name__}")
-    
-    for alg_name, alg_func in algorithms.items():
-        print(f"\tAlgorithm: {alg_name}")
+    for problem_func in problems:
+        problem = problem_func()
+        print(f"Problem: {problem.__class__.__name__}")
         
-        # Run algorithm
-        best_state, best_fitness = alg_func(problem)
-        
-        # Output results
-        print(f"\t\tBest Fitness: {best_fitness}")
+        for alg_name, alg_func in algorithms.items():
+            print(f"\tAlgorithm: {alg_name}")
+            
+            # Run algorithm
+            # best_state, best_fitness = alg_func(problem)
+            result = alg_func(problem)
+
+            if isinstance(result, tuple) and len(result) == 2:
+                best_state, best_fitness = result
+                print(f"\t\tBest Fitness: {best_fitness}")
+            else:
+                print("Unexpected return value:", result)
+            
+            # Output results
+            # print(f"\t\tBest Fitness: {best_fitness}")
+
+if __name__=='__main__':
+    main()
